95ceca3f4a2a4d59844643b1f2e8b06ed66fdf38
mohammadmoradzade <56541124+mohammadmoradzade@users.noreply.github.com>
2019-10-28 11:33:45 +0330

    Add files via upload

---
+++ arkatoast/src/main/java/com/arkadevelop/arkatoast/LogDebug.java
@@ -0,0 +1,13 @@
+package com.arkadevelop.arkatoast;
+
+import android.util.Log;
+
+public class LogDebug {
+
+    private static final String TAG = "SUPER_AWESOME_APP";
+
+    public static void d(String message) {
+        Log.d(TAG, message);
+    }
+
+}
